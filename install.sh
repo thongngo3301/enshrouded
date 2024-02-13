e148242d5c2dfc067e7653b7d314aa5e6a55438e
thongngo3301 <thongngo3301@gmail.com>
2024-02-13 08:29:57 +0000

    update

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 export ENSHROUDED_DIR=/opt/stacks/enshrouded
 curl -fsSL https://raw.githubusercontent.com/thongngo3301/enshrouded/master/.env > ${ENSHROUDED_DIR}/.env
-curl -fsSL https://github.com/thongngo3301/enshrouded/raw/master/docker-compose.yaml > ${ENSHROUDED_DIR}/docker-compose.yaml
+curl -fsSL https://raw.githubusercontent.com/thongngo3301/enshrouded/master/docker-compose.yaml > ${ENSHROUDED_DIR}/docker-compose.yaml
